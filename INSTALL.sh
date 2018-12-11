0c58b6c9e9d4091b5224825b6e19268643123a07
Ivan Nieto <ivan.nieto.sousa@gmail.com>
2018-12-11 17:13:51 +0100

    Fix ansible installer

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-./install-ansible.sh
+sudo apt install python3-pip
+
+./ansible/install-ansible.sh
 
 ansible-playbook ansible/install.yml -e ansible_python_interpreter=/usr/bin/python2.7 --ask-sudo-pass
--- ansible/install-ansible.sh
+++ ansible/install-ansible.sh
@@ -1,23 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Usage Example:
-# ./install_ansible.sh 'v1.1' path/to/local/ansible/hosts/file
-# Look up https://github.com/ansible/ansible for version tags to use
-
-versionTag=$1
-ansibleHosts=$2
-
-# Install Git
-# The most stable way of getting the latest version of ansible to get it directly from the git repo
-echo "[Install Ansible] Install Git"
-sudo apt-get install -y git-core
-
-# Install Ansible Dependencies
-echo '[Install Ansible] Install dependencies'
-sudo apt-get install -y python-setuptools python-apt-common python3-apt
-sudo easy_install jinja2 
-sudo easy_install pyyaml
-sudo easy_install paramiko
-
-# Install Ansible
-pip install ansible
+if ! command -v ansible >/dev/null; then
+        echo "Installing Ansible."
+        sudo apt install ansible -y
+fi
